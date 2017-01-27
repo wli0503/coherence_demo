a65343f96163ba7174a37a25872986530dbadd9c
Wen Li <wli@terakeet.com>
2017-01-26 21:50:38 -0500

    Update dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -37,7 +37,7 @@ config :phoenix, :stacktrace_depth, 20
 config :webapp, Webapp.Repo,
   adapter: Ecto.Adapters.MySQL,
   username: "root",
-  password: "900503",
+  password: "",
   database: "webapp_dev",
   hostname: "localhost",
   pool_size: 10
